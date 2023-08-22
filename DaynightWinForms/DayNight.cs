0fedada3038b88756858d77b907916a7859a779d
Jefferson S. Motta <55891584+jssmotta@users.noreply.github.com>
2023-08-22 17:06:28 -0300

    Version 1.0.0

---
+++ DaynightWinForms/DayNight.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace TelerikWinFormsApp1;
+
+internal class DayNight
+{
+    private bool themeDay;
+    private RadImageButtonElement daynightButton;
+
+    public DayNight(RadForm form)
+    {
+        new Telerik.WinControls.Themes.FluentTheme().DeserializeTheme();
+        new Telerik.WinControls.Themes.FluentDarkTheme().DeserializeTheme();
+         
+        CreateDayNightButton(form);        
+
+        themeDay = Telerik.WinControls.WindowsSettings.AppsUseLightTheme;
+
+        SetDayNight();
+    }
+
+
+    private void CreateDayNightButton(RadForm form)
+    {
+        daynightButton = new RadImageButtonElement
+        {
+            ThemeRole = "TitleBarMinimizeButton",
+            Text = "☾",
+            DisplayStyle = DisplayStyle.Text,
+            ShowBorder = false,
+            AutoSize = false,
+            Size = form.FormElement.TitleBar.MinimizeButton.Size
+        };
+        daynightButton.Click += DayNight_Click;
+        form.FormElement.TitleBar.SystemButtons.Children.Insert(0, daynightButton);
+    }
+
+    private void DayNight_Click(object sender, EventArgs e)
+    {
+        themeDay = !themeDay;
+        SetDayNight();
+    }
+    private void SetDayNight()
+    {
+        if (themeDay)
+        {
+            ThemeResolutionService.ApplicationThemeName = "Fluent";
+            daynightButton.Text = "☾"; 
+        }
+        else
+        {
+            ThemeResolutionService.ApplicationThemeName = "FluentDark";
+            daynightButton.Text = "☼"; 
+        }
+    }
+}
---
+++ DaynightWinForms/RadForm1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using Telerik.WinControls.UI;
+namespace TelerikWinFormsApp1
+{
+    public partial class RadForm1 : RadForm
+    {
+      
+        public RadForm1()
+        {
+            InitializeComponent();
+
+            Text = "My Application - Sales";
+            
+            var grid = new RadGridView();
+            grid.Dock = DockStyle.Fill;
+            var textColumn = new GridViewTextBoxColumn();
+            textColumn.HeaderText = "Name";
+            textColumn.FieldName = "Name";
+            textColumn.Width = 200;
+            grid.Columns.Add(textColumn);
+
+            var sales = new GridViewTextBoxColumn();
+            sales.HeaderText = "Sales";
+            sales.FieldName = "Sales";
+            sales.Width = 200;
+            grid.Columns.Add(sales);
+
+            grid.AllowAddNewRow = false;
+            grid.DataSource = new List<Countries>() { new("Brazil", "US$ 500,000.00"), new("Bulgary", "US$ 1,000,000.00"), new("United States", "US$ 400,000.00"), new("Canada", "US$ 100,000.00"), new("England", "US$ 700,000.00") };
+
+            grid.AllowAutoSizeColumns = true;
+
+            Controls.Add(grid);
+
+            _ = new DayNight(this);
+        }
+
+    }
+    public class Countries
+    {
+        public Countries(string name, string sales)
+        {
+            Name = name;
+            Sales = sales;
+        }
+
+        public string Name { get; set; }
+        public string Sales { get; set; }
+    }
+}
